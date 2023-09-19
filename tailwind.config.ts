0ff20067ffd9cde5bbcddbc7ecaaf490fc0eab88
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2023-09-19 18:38:34 -0400

    section 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        bg1: 'url("/background1.png")',
+        bg3: 'url("/elevateBg.pngs")'
       },
+      colors: {
+        'primary-default': '#4A41C7',
+        'primary-hover': '#1A1467',
+        'primary-disabled': '#827BD7',
+        'primary-warning': '#C74141',
+        'secondary-default': '#262633',
+        'secondary-hover': '#090909',
+        'secondary-disabled': '#262633',
+        'secondary-warning': '#262633',
+      }
     },
+    
   },
   plugins: [],
 }
 export default config
+
