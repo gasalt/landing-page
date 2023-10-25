4ff0628bea5f87d40b1c627e5bd86aee29aab797
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2023-10-24 23:21:09 -0400

    fix basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import Image from "next/image";
 import style from "@/styles/page.module.scss";
-const prefix = ''
+const prefix = "";
 const whyCards = [
   {
     logo: `/pay.svg`,
@@ -68,15 +68,15 @@ const howTos = [
 
 export default function Home() {
   const slideLeft = (event: any) => {
-    event.preventDefault()
-    let slider = document.getElementById('slider')!;
-    slider!.scrollLeft = slider.scrollLeft +=300
+    event.preventDefault();
+    let slider = document.getElementById("slider")!;
+    slider!.scrollLeft = slider.scrollLeft += 300;
   };
 
   const slideRight = (event: any) => {
-    event.preventDefault()
-    let slider = document.getElementById('slider')!;
-   slider.scrollLeft = slider.scrollLeft -=300;
+    event.preventDefault();
+    let slider = document.getElementById("slider")!;
+    slider.scrollLeft = slider.scrollLeft -= 300;
   };
 
   return (
@@ -133,7 +133,7 @@ export default function Home() {
         <p
           className={style.whyDescription}
         >{`While other wallets fall short, we've cracked the code to empower you with seamless DApp interaction on the Ethereum network.`}</p>
-        <div className={style.whyCards} id='slider'>
+        <div className={style.whyCards} id="slider">
           {whyCards.map((card) => (
             <div key={card.logo} className={style.whyCard}>
               <div className={style.whyCardLogo}>
@@ -164,17 +164,6 @@ export default function Home() {
         </div>
       </section>
       <section className={style.elevate}>
-        <div className={style.elevateText}>
-          <p className={style.elevateTitle}>
-            Elevate your DApp experience with Gasalt, the game-changing
-            meta-transaction wallet.
-          </p>
-          <p className={style.elevateNote}>
-            Gasalt not only offers seamless meta-transactions but also empowers
-            you to actively engage with Ethereum DApps.
-          </p>
-          <button className={style.startButton}>Get started</button>
-        </div>
         <div className={style.elevatePhone}>
           <Image
             src={`/top-right.png`}
@@ -198,16 +187,28 @@ export default function Home() {
             className="absolute md:mt-[520px] md:mr-[450px] mr-[160px] mt-[350px] md:w-[281px] md:h-[207px] w-[180px] h-[132px]"
           />
         </div>
+        <div className={style.elevateText}>
+          <p className={style.elevateTitle}>
+            Elevate your DApp experience with Gasalt, the game-changing
+            meta-transaction wallet.
+          </p>
+          <p className={style.elevateNote}>
+            Gasalt not only offers seamless meta-transactions but also empowers
+            you to actively engage with Ethereum DApps.
+          </p>
+          <div className={style.startButton}>
+            <button>Get Started</button>
+          </div>
+        </div>
       </section>
       <section className={style.balance}>
         <div className={style.balancePhone}>
           <Image
             src={`/swap.png`}
-            width={281}
-            height={207}
+            width={311}
+            height={630}
             alt="phone-swap"
-            className="w-[249px] h-[504px] md:mr-15 mr-20"
-            // className="absolute md:mt-[520px] md:mr-[350px] mr-[160px] mt-[350px] md:w-[281px] md:h-[207px] w-[180px] h-[132px]"
+            className="h-[504px] w-[249px] md:h-[630px] md:w-[311px] mr-20"
           />
           <Image
             src={`/qr.png`}
@@ -219,7 +220,7 @@ export default function Home() {
         </div>
         <div className={style.balanceText}>
           <p className={style.balanceTitle}>
-           Discover a world of convenience, flexibility like never before.
+            Discover a world of convenience, flexibility like never before.
           </p>
           <div className={style.balanceList}>
             <>
@@ -231,7 +232,7 @@ export default function Home() {
                     height={32}
                     alt="phone-swap"
                   />
-                  <p className="text-[15px] md:text-[23px] md:leading-[29px] leading-[20px] text-[#BBB8EA] ml-8 my-5">
+                  <p className="text-[20px] w-[302px] md:w-[517px] md:text-[24px] md:leading-[29px] leading-[20px] text-[#BBB8EA] ml-4 my-5">
                     {" "}
                     {feature.text}
                   </p>
@@ -245,6 +246,19 @@ export default function Home() {
       <section className={style.howTo}>
         <p className={style.howToTitle}>Using Gasalt is as Easy as 1-2-3!</p>
         <div className={style.howToBody}>
+        <div className={style.howToPhoto}>
+            <div className={style.howPhotoContainer}>
+              <div className={style.howPhotoContainerImage}>
+                <Image
+                  className="mx-auto md:mt-[220px] mt-[100px] w-[263px] overflow-y-hidden h-[538px]"
+                  src={`/phone-front.svg`}
+                  width={332}
+                  height={673}
+                  alt="iphone"
+                />
+              </div>
+            </div>
+          </div>
           <div className={style.howToText}>
             <>
               {howTos.map((how) => (
@@ -253,16 +267,18 @@ export default function Home() {
                     <div className="w-[40px] h-[40px] bg-[#F4EBFF] flex justify-center items-center rounded-full">
                       <Image
                         src={how.image}
-                        width={20.9}
-                        height={18.23}
+                        width={20}
+                        height={40}
                         alt="phone-swap"
                         className="w-[20.9px] h-[18.23px]"
                       />
                     </div>
                   </div>
                   <div className={style.howList}>
-                    <p className="text-[20px] text-[#101828] md:w-auto w-[280px]">{how.title}</p>
-                    <p className="md:w-auto w-[260px]">{how.note}</p>
+                    <p className={style.howListTitle}>
+                      {how.title}
+                    </p>
+                    <p className={style.howListNote}>{how.note}</p>
                   </div>
                 </div>
               ))}
@@ -272,7 +288,7 @@ export default function Home() {
                   width={157}
                   height={46}
                   alt="app-store-black"
-                  className="mr-10"
+                  // className="mr-10"
                 />
                 <Image
                   src={`/play-store-black.png`}
@@ -284,42 +300,39 @@ export default function Home() {
               </div>
             </>
           </div>
-          <div className={style.howToPhoto}>
-            <div className={style.howPhotoContainer}>
-              <div className="overflow-y-hidden h-[560px]">
-                <Image
-                  className="mx-auto mt-[150px]"
-                  src={`/phone-front.svg`}
-                  width={332}
-                  height={673}
-                  alt="iphone"
-                />
-              </div>
-            </div>
-          </div>
+
         </div>
       </section>
-      <section className={style.footer}>
-        <p className="text-[#FFFFFF] md:text-[32px] text-[22px] text-center pt-16"> Gasalt </p>
-        <p className={style.footerStarted}> {"Let’s get started on something great"} </p>
+      <footer className={style.footer}>
+        <p className="text-[#FFFFFF] text-[32px] text-center pt-16">
+          {" "}
+          Gasalt{" "}
+        </p>
+        <p className={style.footerStarted}>
+          {" "}
+          {"Let’s get started on something great"}{" "}
+        </p>
         <div className="mx-auto w-[310px] pt-10 space-x-10">
-          <button className="bg-primary-default text-[#FFF] w-[134px] h-[48px] rounded-lg">View Demo</button>
-          <button className="transparent border text-[#FFF] border-primary-default w-[134px] h-[48px] rounded-lg">Get started</button>
+          <button className="bg-primary-default text-[#FFF] w-[134px] h-[48px] rounded-lg">
+            View Demo
+          </button>
+          <button className="transparent border text-[#FFF] border-primary-default w-[134px] h-[48px] rounded-lg">
+            Get started
+          </button>
         </div>
-        <div className={style.footerBorder}>
-          <p className={style.rights}>© 2023 Gasalt. All rights reserved.</p>
-          <div className={style.options}>
+        <hr  className={style.footerBorder}/>
+        <div className="md:flex-row flex-col-reverse flex md:w-[1280px] w-[330px] justify-between mt-10 md:mx-auto  text-[16px] text-[#98A2B3]">
+          <p className="text-center ml-16 mt-5 md:mt-0 md:ml-0">© 2023 Gasalt. All rights reserved.</p>
+          <div className="flex md:flex-row flex-col justify-center items-center md:ml-0 ml-20 space-y-5 md:space-y-0 md:space-x-5 leading-[24px]">
             <p>Email</p>
             <p>Github</p>
-            <div className="flex space-x-5 md:relative md:right-0 relative right-10">
-            <p>Terms</p>
-            <p>Privacy</p>
+            <div className="flex space-x-5">
+              <p>Terms</p>
+              <p>Privacy</p>
             </div>
-            
           </div>
         </div>
-
-      </section>
+      </footer>
     </main>
   );
 }
